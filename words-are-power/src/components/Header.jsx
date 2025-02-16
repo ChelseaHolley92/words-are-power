b744695a0f6f392ffac5f7968cb88972ef85905e
Your Username <ChelseaHolley23@gmail.com>
2025-02-16 09:15:01 -0500

    commit

---
+++ words-are-power/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="/applogo.png" alt="Words Are Power Logo" className="logo" />
+            <h1>Words Are Power Affirmation App</h1>
+        </header>
+    );
+
+};
+
+export default Header;
