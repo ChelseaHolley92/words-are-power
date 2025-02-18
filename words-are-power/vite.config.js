ec1955e8cd21789c6c8a977a0f042dc6f0d4d8aa
Your Username <ChelseaHolley23@gmail.com>
2025-02-17 20:04:48 -0500

    commit

--- words-are-power/vite.config.js
+++ words-are-power/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/words-are-power/',
+  base: process.env.NODE_ENV==='production'? '/words-are-power/' :'/',
   plugins: [react()],
 });
